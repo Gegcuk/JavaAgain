5ae775e67ed29ac3bb39591cba9105c6895d7029
gegcuk <gegcuk@gmail.com>
2022-03-26 23:46:45 +0300

    hello world

--- src/FirstJavaClass.java
+++ src/FirstJavaClass.java
@@ -2,6 +2,12 @@ public class FirstJavaClass {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        helloWorld();
+    }
+
+    public static void helloWorld(){
+        System.out.println("Hello World");
+        System.out.println("Hello Java");
     }
 
 }
