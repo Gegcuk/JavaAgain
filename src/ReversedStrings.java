e10ee35c56fa06d7da1c2cce03b637f423865723
Aleksey Lazunin <gegcuk@gmail.com>
2022-05-29 21:43:39 +0300

    ReversedString

--- src/ReversedStrings.java
+++ src/ReversedStrings.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class ReversedStrings {
+public class ReversedStrings {
+
+    public static void main(String[] args) {
+        System.out.println(Kata.solution("drow"));
+    }
+}
+
+class Kata {
+
+    public static String solution(String str) {
+
+        return new StringBuilder(str).reverse().toString();
+    }
+
 }
