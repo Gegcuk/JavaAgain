23f96cf25935b4ca36313c2b5fe32622ecca105e
Aleksey Lazunin <gegcuk@gmail.com>
2022-06-28 20:48:03 +0300

    FindMissing

---
+++ src/MissingInArithmeticProgression.java
@@ -0,0 +1,51 @@
+import java.util.Arrays;
+import java.util.Collections;
+
+public class MissingInArithmeticProgression {
+    public static void main(String[] args) {
+        findMissing(new int[]{ 1, 3, 4});
+        findMissing(new int[]{ 1, 2, 4});
+        findMissing(new int[]{ 4, 2, 1});
+        findMissing(new int[]{ 4, 3, 1});
+        findMissing(new int[]{ 4, 2, 1});
+        findMissing(new int[]{ 1, 1, 1});
+    }
+
+    public static int findMissing(int[] numbers)
+    {
+        int diff= 0;
+        int[] temp = new int[numbers.length];
+
+        if(numbers[1] < numbers[0]){
+            for(int i = 0, j = numbers.length-1; i < numbers.length; i++, j--){
+                temp[i] = numbers[j];
+            }
+            numbers = temp;
+        }
+
+
+        for(int i = 1; i < numbers.length-1; i++){
+            if (numbers[i] - numbers[i-1] == numbers[i+1] - numbers[i]){
+                diff = numbers[i] - numbers[i-1];
+                break;
+            } else if(numbers[i] - numbers[i-1] < numbers[i+1] - numbers[i]){
+                    diff = numbers[i] - numbers[i-1 ];
+                    break;
+                }
+            else {
+                diff = numbers[i + 1] - numbers[i];
+                break;
+            }
+        }
+
+        for(int i = 1; i < numbers.length; i++){
+            if (numbers[i] - numbers[i-1] != diff){
+                System.out.println("Diff = " + diff);
+                System.out.println(numbers[i] - diff);
+                return numbers[i] - diff;
+            }
+        }
+
+        return numbers[0];
+    }
+}
