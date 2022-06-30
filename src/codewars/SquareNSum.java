2f01bf662c74d943fb1e1f957488bd74bca2cec5
Aleksey Lazunin <gegcuk@gmail.com>
2022-06-30 17:57:48 +0300

    SquareNSum

---
+++ src/codewars/SquareNSum.java
@@ -0,0 +1,19 @@
+package codewars;
+
+import java.util.Arrays;
+
+public class SquareNSum {
+
+
+    public static void main(String[] args) {
+        System.out.println(squareSum(new int[] {1,2,2}));
+        System.out.println(squareSum(new int[] {1,2}));
+        System.out.println(squareSum(new int[] {5, -3, 4}));
+        System.out.println(squareSum(new int[] {}));
+    }
+
+    public static int squareSum(int[] n)
+    {
+        return Arrays.stream(n).map(i -> i*i).sum();
+    }
+}
