023ee0d0a8091153a910f60fd2111dd36d1bae10
Aleksey Lazunin <gegcuk@gmail.com>
2022-06-30 17:34:08 +0300

    CountingSheeps

---
+++ src/codewars/CountingSheeps.java
@@ -0,0 +1,23 @@
+package codewars;
+
+public class CountingSheeps {
+
+    public static void main(String[] args) {
+        System.out.println(countingSheep(0));
+        System.out.println(countingSheep(1));
+        System.out.println(countingSheep(2));
+        System.out.println(countingSheep(3));
+    }
+
+    public static String countingSheep(int num) {
+        StringBuilder result = new StringBuilder();
+        if(num > 0) {
+            for(int i = 1; i <= num; i++){
+                result.append(i + " sheep...");
+            }
+        } else {
+            return "";
+        }
+        return result.toString();
+    }
+}
