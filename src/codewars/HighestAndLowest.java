e589d7ae9193f46a4220ffe77eb3d786a8d5cd67
Aleksey Lazunin <gegcuk@gmail.com>
2022-06-30 16:38:47 +0300

    refactor classes places. Moved 2 classes to codewars folder

--- src/codewars/HighestAndLowest.java
+++ src/codewars/HighestAndLowest.java
@@ -1,4 +1,4 @@
-//In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
+package codewars;//In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
 //
 //        Examples
 //        highAndLow("1 2 3 4 5")  // return "5 1"
