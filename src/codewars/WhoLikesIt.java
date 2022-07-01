e4309a91613c03075a1a2f508cef5855409cea7c
Aleksey Lazunin <gegcuk@gmail.com>
2022-07-01 22:58:09 +0300

    correct string formatting

--- src/codewars/WhoLikesIt.java
+++ src/codewars/WhoLikesIt.java
@@ -24,7 +24,7 @@ public class WhoLikesIt {
         switch (names.length){
             case 0: return "no one likes this";
             case 1: return String.format("%s likes this", names[0]);
-            case 2: return String.format("%s and $s like this", names[0], names[1]);
+            case 2: return String.format("%s and %s like this", names[0], names[1]);
             case 3: return String.format("%s, %s and %s like this", names[0], names[1], names[2]);
             default: return String.format("%s, %s and %d other like this", names[0], names[1], names.length-2);
         }
