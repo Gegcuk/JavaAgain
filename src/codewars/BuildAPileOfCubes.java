34bd4a7bc21b87a99bcf34904f9f30d70f24085c
Aleksey Lazunin <gegcuk@gmail.com>
2022-07-02 13:22:04 +0300

    Build a pile of cubes

---
+++ src/codewars/BuildAPileOfCubes.java
@@ -0,0 +1,26 @@
+package codewars;
+
+public class BuildAPileOfCubes {
+
+    public static void main(String[] args) {
+        System.out.println(findNb(40539911473216L));
+    }
+
+
+    public static long findNb(long m) {
+        long sum = 0;
+        long iteration = 0;
+        while (true){
+            if (sum == m) {
+                return iteration;
+            } else if (sum < m) {
+                iteration++;
+                sum += cubeOfNumber(iteration);
+            } else return -1;
+        }
+    }
+
+    private static long cubeOfNumber(long number){
+        return number*number*number;
+    }
+}
