38ef4aa7da1a5cdac956a24ec4cf1b98d9bcf02f
Aleksey Lazunin <gegcuk@gmail.com>
2022-07-02 14:39:23 +0300

    Convert string to array

---
+++ src/codewars/kyu8/ConvertAStringToAnArray.java
@@ -0,0 +1,11 @@
+package codewars.kyu8;
+
+import java.util.Arrays;
+
+public class ConvertAStringToAnArray {
+
+    public static String[] stringToArray(String s) {
+        return s.split(" ");
+    }
+
+}
