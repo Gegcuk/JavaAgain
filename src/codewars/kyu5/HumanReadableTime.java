3e51aaf1ed6f587c4c1cc884eaae44b615d1d8e1
gegcuk <gegcuk@gmail.com>
2022-07-14 18:02:44 +0300

    Human readable time optimized

--- src/codewars/kyu5/HumanReadableTime.java
+++ src/codewars/kyu5/HumanReadableTime.java
@@ -4,15 +4,24 @@ public class HumanReadableTime {
 
     public static void main(String[] args) {
         System.out.println(makeReadable(0));
+        System.out.println(makeReadableOptimized(0));
         System.out.println(makeReadable(5));
+        System.out.println(makeReadableOptimized(5));
         System.out.println(makeReadable(60));
+        System.out.println(makeReadableOptimized(60));
         System.out.println(makeReadable(86399));
+        System.out.println(makeReadableOptimized(86399));
         System.out.println(makeReadable(359999));
+        System.out.println(makeReadableOptimized(359999));
     }
 
         public static String makeReadable(int seconds) {
             // Do something
             return String.format("%s:%s:%s", getHours(seconds), getMinutes(seconds), getSeconds(seconds));
+        }
+
+        public static String makeReadableOptimized(int seconds){
+        return String.format("%02d:%02d:%02d", seconds/3600, (seconds/60) % 60, seconds%60);
         }
 
         private static String getSeconds(int seconds) {
